3c2628e1e78d3c9a19809c2ca4d43159eb87c39a
Emir Mertoğlu <emir.mertoglu.97@gmail.com>
2023-06-23 13:32:59 +0300

    header and card components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Header from "@/components/Header";
+import Cards from "@/components/Cards";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="container mx-auto py-6 px-4 max-w-[1200px] h-screen text-center">
+      <Header />
+      <Cards />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Cards/index.jsx
@@ -0,0 +1,43 @@
+const Cards = () => {
+  return (
+    <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-10">
+      <div className="py-6 px-4 flex flex-col bg-blue-200 border-b-8 border-b-blue-500 rounded-md shadow">
+        <span>Infected</span>
+        <span>41,810,387</span>
+        <span>Last updated at:</span>
+        <span>Fri sep 17 2021</span>
+        <span>Number of active cases of COVID-19</span>
+        <span>US</span>
+      </div>
+
+      <div className="py-6 px-4 flex flex-col bg-green-200 border-b-8 border-b-green-500 rounded-md shadow">
+        <span>Infected</span>
+        <span>41,810,387</span>
+        <span>Last updated at:</span>
+        <span>Fri sep 17 2021</span>
+        <span>Number of active cases of COVID-19</span>
+        <span>US</span>
+      </div>
+
+      <div className="py-6 px-4 flex flex-col bg-red-200 border-b-8 border-b-red-500 rounded-md shadow">
+        <span>Infected</span>
+        <span>41,810,387</span>
+        <span>Last updated at:</span>
+        <span>Fri sep 17 2021</span>
+        <span>Number of active cases of COVID-19</span>
+        <span>US</span>
+      </div>
+
+      <div className="py-6 px-4 flex flex-col bg-yellow-200 border-b-8 border-b-yellow-500 rounded-md shadow">
+        <span>Infected</span>
+        <span>41,810,387</span>
+        <span>Last updated at:</span>
+        <span>Fri sep 17 2021</span>
+        <span>Number of active cases of COVID-19</span>
+        <span>US</span>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
