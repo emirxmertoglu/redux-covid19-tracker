e51c1c7b40d56861587b0dd8614fa2df8c0bec78
Emir Mertoğlu <emir.mertoglu.97@gmail.com>
2023-06-24 23:32:15 +0300

    add countries component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Header from "@/components/Header";
 import Cards from "@/components/Cards";
+import Countries from "@/components/Countries";
 
 function App() {
   return (
     <div className="container mx-auto py-6 px-4 max-w-[1200px] h-screen text-center">
       <Header />
-      <Cards />
+      <div className="mb-8">
+        <Cards />
+      </div>
+      <div className="mb-8">
+        <Countries />
+      </div>
     </div>
   );
 }
---
+++ src/components/Countries/index.jsx
@@ -0,0 +1,10 @@
+const Countries = () => {
+  return (
+    <select className="py-2 w-64 border-b-2 border-b-slate-500">
+      <option value="US">US</option>
+      <option value="TUR">Turkey</option>
+    </select>
+  );
+};
+
+export default Countries;
